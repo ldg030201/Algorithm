1983000da8b892fd91d55aa48c884e4a62ca39ef
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-11-18 11:17:57 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if (a > b) {
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
