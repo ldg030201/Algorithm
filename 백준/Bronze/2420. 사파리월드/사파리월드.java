c9ce4e92a388cceaf66817937e1ff9350a82f93b
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-11-18 13:12:04 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(Math.abs(a - b)));
+        bw.flush();
+        bw.close();
+    }
+}
