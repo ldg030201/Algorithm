928a0bba2bf99fecc89bdda73520e19d66649e75
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-11-20 08:48:19 +0900

    [Bronze V] Title: 나는 누구인가, Time: 92 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("I'm Sexy");
+        bw.flush();
+        bw.close();
+    }
+}
