bd43a32bc75791d544c32c61b861d07e78836873
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-11-20 11:15:54 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14456 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,20 +1,22 @@
 import java.io.*;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine(); //값 버림
         int count = 0;
-        int sum = 0;
+        BigDecimal sum = new BigDecimal(0);
+        final BigDecimal to = new BigDecimal(31);
         for (char c : br.readLine().toCharArray()) {
             int charNum = (c - 'a') + 1;
-            sum += (int) (charNum * Math.pow(31, count));
+            sum = sum.add(to.pow(count).multiply(new BigDecimal(charNum)));
             count++;
         }
         br.close();
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(String.valueOf(sum));
+        bw.write(String.valueOf(sum.remainder(BigDecimal.valueOf(1234567891))));
         bw.flush();
         bw.close();
     }
