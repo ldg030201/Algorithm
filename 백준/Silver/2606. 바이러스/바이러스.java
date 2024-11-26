a431c111fb33ba18f25aa7dcf50bc8e2cf970ef2
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-11-26 14:25:20 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,11 +1,10 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static boolean[][] map;
     static boolean[] visited;
+    static int resultCount;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,6 +13,7 @@ public class Main {
 
         map = new boolean[computerCount + 1][computerCount + 1];
         visited = new boolean[computerCount + 1];
+        resultCount = 0;
 
         while (lineCount-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -23,27 +23,18 @@ public class Main {
             map[n1][n2] = true;
             map[n2][n1] = true;
         }
-
-        System.out.println(fnBfs(1));
+        fnDfs(1);
+        System.out.println(resultCount - 1);
     }
 
-    private static int fnBfs(int startId) {
-        int resultCount = 0;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(startId);
-        visited[startId] = true;
+    private static void fnDfs(int id) {
+        visited[id] = true;
+        resultCount++;
 
-        while (!queue.isEmpty()) {
-            int q = queue.poll();
-            for (int i = 1; i <= map[q].length - 1; i++) {
-                if (!visited[i] && map[q][i]) {
-                    visited[i] = true;
-                    queue.add(i);
-                    resultCount++;
-                }
+        for (int i = 1; i <= map[id].length - 1; i++) {
+            if (!visited[i] && map[id][i]) {
+                fnDfs(i);
             }
         }
-
-        return resultCount;
     }
 }
