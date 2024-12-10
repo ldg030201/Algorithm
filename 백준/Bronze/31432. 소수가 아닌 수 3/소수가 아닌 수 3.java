361955ed31e42ef95a47fb0454ace6b4d525a821
lee dong geon <100822592+ldg030201@users.noreply.github.com>
2024-12-10 09:12:53 +0900

    [Bronze I] Title: 소수가 아닌 수 3, Time: 100 ms, Memory: 14236 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31432. 소수가 아닌 수 3/소수가 아닌 수 3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println("YES");
+        System.out.println(Integer.parseInt(st.nextToken()) * 111);
+    }
+}
