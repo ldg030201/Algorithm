4ee208dbd24c0beb2b2b1a9d708efc6fe7ab7b98
Lee Dong Geon <100822592+ldg030201@users.noreply.github.com>
2026-01-21 00:15:03 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+        bw.flush();
+    }
+}
